131913c6ed27002c2c9492453bf1eae44d28c2a4
sagar2202 <sagar.s.jain@gmail.com>
2017-01-10 15:12:32 -0800

    update

--- PaymentService/src/main/java/demo/microservice/payment/PaymentServiceApplication.java
+++ PaymentService/src/main/java/demo/microservice/payment/PaymentServiceApplication.java
@@ -2,7 +2,10 @@ package demo.microservice.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @author Sagar Jain
+ *
+ */
 @SpringBootApplication
 public class PaymentServiceApplication {
 
